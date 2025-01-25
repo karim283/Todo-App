3f4140428206ef1439aa296e50266c370b1ba719
karim emad <sim.KarimEmad69873@alexu.edu.eg>
2025-01-25 18:29:37 +0200

    tailwind update

--- src/todo.jsx
+++ src/todo.jsx
@@ -33,7 +33,7 @@ export default function Todo() {
               handleAddTodo();
             }
           }}
-          className="rounded-[6px] w-[644px] h-14 bg-[#262626] border-[1px] border-[#000000] text-white text-2xl p-4 text-[16px] text-[#808080]]"
+          className="rounded-[6px] w-[50.33vw] h-14 bg-[#262626] border-[1px] border-[#000000] text-white text-2xl p-4 text-[16px] text-[#808080]]"
         />
         <button
           onClick={handleAddTodo}
@@ -42,45 +42,47 @@ export default function Todo() {
           Add <img src={plus} alt="Add" />
         </button>
       </div>
-      <div className="flex justify-center items-center gap-[600px] mt-11">
-        <div className="flex justify-start items-start gap-2">
-          <p className="text-[#4EA8DE] text-[14px]">Tasks</p>
+      <div className="flex justify-center items-center gap-[45.56vw] mt-11 ml-5 mr-5">
+        <div className="flex justify-center items-center gap-2">
+          <p className="text-[#4EA8DE] text-[1rem]">Tasks</p>
           <p className="text-white rounded-[999px] text-center text-[12px] w-6 h-5 bg-[#333333]">
             {todos.length}
           </p>
         </div>
-        <div className="flex justify-start items-start gap-2">
-          <p className="text-[#8284FA] text-[14px]">Done</p>
+        <div className="flex justify-center items-center gap-2">
+          <p className="text-[#8284FA] text-[1rem]">Done</p>
           <p className="text-white rounded-[999px] text-center text-[12px] w-12 h-5 bg-[#333333]">
             {todos.filter((todo) => todo.completed).length} of {todos.length}
           </p>
         </div>
       </div>
-      <div className="ml-[390px]">
+      <div className="flex flex-col justify-center items-center ">
         {todos.map((todo) => (
           <div
             key={todo.id}
-            className="w-[736px] h-[72px] bg-[#262626] rounded-[8px] border-[1px] border-[#333333] flex justify-center items-center mt-6"
+            className="w-[20rem]  md:w-[50.33vw] h-full bg-[#262626] rounded-[8px] border-[1px] border-[#333333] flex justify-start items-center mt-6  "
           >
-            <img
-              src={todo.completed ? checked : uncheck}
-              alt={todo.completed ? "Checked" : "Unchecked"}
-              className="relative right-4 bottom-2 cursor-pointer"
-              onClick={() => toggleTodo(todo.id)}
-            />
-            <p
-              className={`text-[14px] p-4 w-[632px] relative right-4 ${
-                todo.completed
-                  ? "text-[#808080] line-through"
-                  : "text-[#F2F2F2]"
-              }`}
-            >
-              {todo.text}
-            </p>
+            <div className="flex justify-start items-center">
+              <img
+                src={todo.completed ? checked : uncheck}
+                alt={todo.completed ? "Checked" : "Unchecked"}
+                className="pl-4 pr-4 cursor-pointer"
+                onClick={() => toggleTodo(todo.id)}
+              />
+              <p
+                className={`text-[14px] w-60 pt-5 pb-5 md:w-[40vw]  ${
+                  todo.completed
+                    ? "text-[#808080] line-through"
+                    : "text-[#F2F2F2]"
+                }`}
+              >
+                {todo.text}
+              </p>
+            </div>
             <img
               src={delet}
               alt="Delete"
-              className="mb-4 cursor-pointer"
+              className=" pl-4 pr-4 cursor-pointer  "
               onClick={() => deleteTodo(todo.id)}
             />
           </div>
